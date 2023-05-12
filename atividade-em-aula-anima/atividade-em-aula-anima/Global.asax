c90eef1e43a9f396782091edf4c9b98472a8b197
andersonayres <andersongonzaga82@gmail.com>
2023-05-12 10:07:33 -0300

    Anderson primeira parte.

---
+++ atividade-em-aula-anima/atividade-em-aula-anima/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="atividade_em_aula_anima.MvcApplication" Language="C#" %>
---
+++ atividade-em-aula-anima/atividade-em-aula-anima/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas através do seguinte
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("atividade_em_aula_anima")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Centro universitário Ritter dos Reis")]
+[assembly: AssemblyProduct("atividade_em_aula_anima")]
+[assembly: AssemblyCopyright("Copyright © Centro universitário Ritter dos Reis 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como falso torna não visíveis os tipos neste assembly
+// para componentes COM. Caso precise acessar um tipo neste assembly a partir de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("ef76025d-44e0-48c8-9307-5fcc33e91639")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Major Version
+//      Versão Secundária
+//      Build Number
+//      Revision
+//
+// É possível especificar todos os valores ou definir como padrão os números de revisão e de versão
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
